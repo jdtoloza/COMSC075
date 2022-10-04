2f370fa48dfa41c19d333815007056cf15aae067
Josh Toloza <90005135+jdtoloza@users.noreply.github.com>
2022-10-03 23:13:56 -0700

    Create Execise22.java

---
+++ Execise22.java
@@ -0,0 +1,12 @@
+public class Exercise22{
+	
+	public static void main (String[] args) {
+		double temperature = 17;
+		double relHumidity = 30;
+		
+		double dewPoint = temperature - ((100 - relHumidity) / 5);
+		System.out.printf("For air temperature of %.1f degrees Celsius \n", temperature);
+		System.out.printf("and relative humidity of %.1f%% \n", relHumidity);
+		System.out.printf("The dew point is %.1f degrees Celsius \n", dewPoint);
+	}
+}
